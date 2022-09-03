f486598054cbd3419f14ecc0a37e53d07a36b33c
evollt <levniko343@gmail.com>
2022-09-03 03:18:22 +0300

    test

--- controllers/db.js
+++
@@ -1,7 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://cluster0.c6csvjf.mongodb.net/myFirstDatabase";
-
-MongoClient.connect(url, function(err, db) {
-  console.log("Database created!");
-  db.close();
-});
